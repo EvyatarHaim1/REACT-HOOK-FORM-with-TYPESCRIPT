517cf9865e8badcbbc5a52c847fb081a5d24d859
Evyatar Haim <evyataros1@gmail.com>
2022-01-16 16:05:19 +0200

    adding maxLength and project title

--- src/App.tsx
+++ src/App.tsx
@@ -22,56 +22,59 @@ function App() {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      style={{ textAlign: "center", marginTop: "200px" }}>
-      <div>
-        <label htmlFor="firstname">First name</label>
-        <br />
-        <input
-          {...register("firstname", { required: true })}
-          type="text"
-          id="firstname"
-          name="firstname"
-        />
-        {errors.firstname && (
-          <div style={{ color: "red" }} className="error">
-            Enter your first name
-          </div>
-        )}
-      </div>
-      <div>
-        <label htmlFor="lastname">Last name</label>
-        <br />
-        <input
-          {...register("lastname", { required: true, maxLength: 20 })}
-          type="text"
-          id="lastname"
-          name="lastname"
-        />
-        {errors.lastname && (
-          <div style={{ color: "red" }} className="error">
-            Enter your last name
-          </div>
-        )}
-      </div>
-      <div>
-        <label htmlFor="age">Age</label>
-        <br />
-        <input
-          {...register("age", { required: true, maxLength: 3 })}
-          type="text"
-          id="age"
-          name="age"
-        />
-        {errors.age && (
-          <div style={{ color: "red" }} className="error">
-            Enter your age
-          </div>
-        )}
-      </div>
-      <button type="submit">Save</button>
-    </form>
+    <div style={{ textAlign: "center", marginTop: "200px" }}>
+      <h1>
+        Validate form with React-hook-form library <br /> React-Typescript
+      </h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="firstname">First name</label>
+          <br />
+          <input
+            {...register("firstname", { required: true, maxLength: 20 })}
+            type="text"
+            id="firstname"
+            name="firstname"
+          />
+          {errors.firstname && (
+            <div style={{ color: "red" }} className="error">
+              Enter your first name
+            </div>
+          )}
+        </div>
+        <div>
+          <label htmlFor="lastname">Last name</label>
+          <br />
+          <input
+            {...register("lastname", { required: true, maxLength: 20 })}
+            type="text"
+            id="lastname"
+            name="lastname"
+          />
+          {errors.lastname && (
+            <div style={{ color: "red" }} className="error">
+              Enter your last name
+            </div>
+          )}
+        </div>
+        <div>
+          <label htmlFor="age">Age</label>
+          <br />
+          <input
+            {...register("age", { required: true, maxLength: 3 })}
+            type="text"
+            id="age"
+            name="age"
+          />
+          {errors.age && (
+            <div style={{ color: "red" }} className="error">
+              Enter your age
+            </div>
+          )}
+        </div>
+        <button type="submit">Save</button>
+      </form>
+    </div>
   );
 }
 
